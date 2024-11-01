1c68b9db4088d6f6bed0e386d1af642a86766972
Francisco Hernandez Vivanco <hernandf@ansto.gov.au>
2024-11-01 11:20:01 +1100

    allow Path in LoadHDF5File pydantic model

--- ansto_simplon_api/schemas/ansto_endpoints.py
+++ ansto_simplon_api/schemas/ansto_endpoints.py
@@ -1,9 +1,10 @@
+from pathlib import Path
 from typing import Literal
 
 from pydantic import BaseModel, Field
 
 
 class LoadHDF5File(BaseModel):
-    hdf5_file_path: str = Field(examples=["/path/to/master_file"])
+    hdf5_file_path: str | Path = Field(examples=["/path/to/master_file"])
     number_of_datafiles: int | None = Field(default=None, examples=[1])
     compression: Literal["bslz4", "none"] = Field(default="bslz4", examples=["bslz4"])
